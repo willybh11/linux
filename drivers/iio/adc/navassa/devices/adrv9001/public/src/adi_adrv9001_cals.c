ad6ea3fcaae8e6ec0a20ad5d5b05ccbda720c8d4
willybh11 <willhagele@gmail.com>
2023-03-27 12:31:49 -0400

    masked LSB with 0x08

--- drivers/iio/adc/navassa/devices/adrv9001/public/src/adi_adrv9001_cals.c
+++ drivers/iio/adc/navassa/devices/adrv9001/public/src/adi_adrv9001_cals.c
@@ -77,7 +77,7 @@ int32_t adi_adrv9001_cals_InitCals_Run(adi_adrv9001_Device_t *adrv9001,
 
 
     /* Bit mask info for Rx2 and Tx2 channels */
-    // payloadMailbox[8]  = (uint8_t)(initCals->chanInitCalMask[ADRV9001_RX2_TX2]);
+    payloadMailbox[8]  = (uint8_t)(initCals->chanInitCalMask[ADRV9001_RX2_TX2]) | 0x08;
         // failed on 0x01
         // failed on 0x02 
         // failed on 0x04 
@@ -85,16 +85,15 @@ int32_t adi_adrv9001_cals_InitCals_Run(adi_adrv9001_Device_t *adrv9001,
         //          failed on 0x30, try 0x10 and 0x20
 
 
-    payloadMailbox[9]  = (uint8_t)(initCals->chanInitCalMask[ADRV9001_RX2_TX2] >> 8) | 0x80;  
+    payloadMailbox[9]  = (uint8_t)(initCals->chanInitCalMask[ADRV9001_RX2_TX2] >> 8) | 0x11;  
         // passed on 0x01
         // failed on 0x02
         // failed on 0x04
         // failed on 0x08 
-        // failed on 0xF0 
         // passed on 0x10
         // failed on 0x20
         // failed on 0x40
-        // 0x80
+        // failed on 0x80
         
     payloadMailbox[10] = (uint8_t)(initCals->chanInitCalMask[ADRV9001_RX2_TX2] >> 16); 
     payloadMailbox[11] = (uint8_t)(initCals->chanInitCalMask[ADRV9001_RX2_TX2] >> 24);
