06b62366726a71e37f306e8947d4572d207b8f90
willybh11 <willhagele@gmail.com>
2023-03-27 12:06:10 -0400

    0x40

--- drivers/iio/adc/navassa/devices/adrv9001/public/src/adi_adrv9001_cals.c
+++ drivers/iio/adc/navassa/devices/adrv9001/public/src/adi_adrv9001_cals.c
@@ -85,13 +85,15 @@ int32_t adi_adrv9001_cals_InitCals_Run(adi_adrv9001_Device_t *adrv9001,
         //          failed on 0x30, try 0x10 and 0x20
 
 
-    payloadMailbox[9]  = (uint8_t)(initCals->chanInitCalMask[ADRV9001_RX2_TX2] >> 8) | 0x20;  
+    payloadMailbox[9]  = (uint8_t)(initCals->chanInitCalMask[ADRV9001_RX2_TX2] >> 8) | 0x40;  
         // passed on 0x01
         // failed on 0x02
         // failed on 0x04
         // failed on 0x08 
         // failed on 0xF0
-        // 0x10
+        // passed on 0x10
+        // 0x20
+        // x040
         
     payloadMailbox[10] = (uint8_t)(initCals->chanInitCalMask[ADRV9001_RX2_TX2] >> 16); 
     payloadMailbox[11] = (uint8_t)(initCals->chanInitCalMask[ADRV9001_RX2_TX2] >> 24);
